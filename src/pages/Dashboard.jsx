a0bc164e2a6e6c4c13ea85431a22c4b33e06569d
parthraj-g0hil <parthraj.learn@gmail.com>
2025-07-28 15:07:26 +0530

    updates

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <h1>Welcome to Document Management System</h1>;
+}
+export default Dashboard;
+
---
+++ src/pages/Upload.jsx
@@ -0,0 +1,5 @@
+function Upload() {
+  return <h1>Welcome to Document Management System</h1>;
+}
+export default Upload;
+
